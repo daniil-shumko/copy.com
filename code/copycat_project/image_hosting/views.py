de48cd02ed4433eebc54702f55635d149327ddae
daniil-shumko <daniil.shumko@gmail.com>
2015-03-16 22:56:46 +0000

    templates dir added

--- code/copycat_project/image_hosting/views.py
+++ code/copycat_project/image_hosting/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("copyCAT")
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    return render(request, 'index.html', context_dict)
