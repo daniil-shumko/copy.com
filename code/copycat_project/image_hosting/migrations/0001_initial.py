7d36e1383d301b80a6ebe155f4d6bf897d175b59
daniil-shumko <daniil.shumko@gmail.com>
2015-03-17 18:32:17 +0000

    models and database created

---
+++ code/copycat_project/image_hosting/migrations/0001_initial.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=128)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Image',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('caption', models.CharField(max_length=128)),
+                ('url_image_name', models.CharField(unique=True, max_length=128)),
+                ('timestamp', models.DateTimeField()),
+                ('views', models.IntegerField(default=0)),
+                ('up_votes', models.IntegerField(default=0)),
+                ('down_votes', models.IntegerField(default=0)),
+                ('category', models.ForeignKey(to='image_hosting.Category')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- code/copycat_project/image_hosting/models.py
+++ code/copycat_project/image_hosting/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __unicode__(self):  #For Python 2, use __str__ on Python 3
+        return self.name
+
+class Image(models.Model):
+    category = models.ForeignKey(Category)
+    caption = models.CharField(max_length=128)
+    url_image_name = models.CharField(max_length=128, unique=True)
+    timestamp = models.DateTimeField()
+    views = models.IntegerField(default=0)
+    up_votes = models.IntegerField(default=0)
+    down_votes = models.IntegerField(default=0)
+
+    def __unicode__(self):      #For Python 2, use __str__ on Python 3
+        return self.url_image_name
