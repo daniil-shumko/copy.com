5f529dbebecd7c84f6798ead7e07a47790c438b3
daniil-shumko <daniil.shumko@gmail.com>
2015-03-20 13:33:38 +0000

    updated admin view to display caption and category

--- code/copycat_project/image_hosting/admin.py
+++ code/copycat_project/image_hosting/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from image_hosting.models import Category, Image
 
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('url_image_name', 'caption', 'category')
+
+
 admin.site.register(Category)
-admin.site.register(Image)
+admin.site.register(Image, ImageAdmin)
