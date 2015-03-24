b52f380bed030cbbe2b7fb3dbc35cfcc1f97f6d2
daniil-shumko <daniil.shumko@gmail.com>
2015-03-24 17:00:31 +0000

    empty database

--- code/copycat_project/image_hosting/migrations/0001_initial.py
+++ code/copycat_project/image_hosting/migrations/0001_initial.py
@@ -25,8 +25,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('caption', models.CharField(max_length=128)),
-                ('url_image_name', models.CharField(unique=True, max_length=128)),
-                ('timestamp', models.DateTimeField()),
+                ('image', models.ImageField(upload_to=b'images/')),
+                ('timestamp', models.DateTimeField(auto_now=True)),
                 ('views', models.IntegerField(default=0)),
                 ('up_votes', models.IntegerField(default=0)),
                 ('down_votes', models.IntegerField(default=0)),
