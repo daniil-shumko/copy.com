3ba438d64cf57ef67b4d21de6838618cf361b897
daniil-shumko <daniil.shumko@gmail.com>
2015-03-29 23:02:36 +0100

    added tests for models

--- code/copycat_project/image_hosting/tests.py
+++ code/copycat_project/image_hosting/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from datetime import *
+from image_hosting.models import Category, Image
 
-# Create your tests here.
+
+class CategoryMethodTests(TestCase):
+    def test_ensure_views_are_positive(self):
+        cat = Category(name='test')
+        cat.save()
+        self.assertEqual((cat.name == 'test'), True)
+
+
+class ImageMethodTests(TestCase):
+    def test_ensure_caption_is_set(self):
+        cat = Category(name='test2')
+        cat.save()
+        image = Image(category=cat)
+        image.caption = 'test caption'
+        image.save()
+        self.assertEqual((image.caption == 'test caption'), True)
+
+    def test_ensure_image_is_set(self):
+        cat = Category(name='test3')
+        cat.save()
+        image = Image(category=cat)
+        image.image = 'images/img.jpg'
+        image.save()
+        self.assertEqual((image.image == 'images/img.jpg'), True)
+
+    def test_ensure_views_are_set(self):
+        cat = Category(name='test4')
+        cat.save()
+        image = Image(category=cat)
+        image.views = 12
+        image.save()
+        self.assertEqual((image.views == 12), True)
+
+    def test_ensure_upvotes_are_set(self):
+        cat = Category(name='test5')
+        cat.save()
+        image = Image(category=cat)
+        image.up_votes = 9
+        image.save()
+        self.assertEqual((image.up_votes == 9), True)
+
+    def test_ensure_downvotes_are_set(self):
+        cat = Category(name='test6')
+        cat.save()
+        image = Image(category=cat)
+        image.down_votes = 11
+        image.save()
+        self.assertEqual((image.down_votes == 11), True)
+
